e039dbdb665c5895e6759b44c622a000ae36d070
mcarolmartins <carol-martins7@hotmail.com>
2021-03-18 13:02:46 -0300

    correção width da table

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,8 @@
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
+import { Container } from './styles';
 
-import { Container } from "../Summary/styles";
 
 export function Summary(){
   return (
