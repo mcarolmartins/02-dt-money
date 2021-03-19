21691c1d187d227dbb1499c7b29970e19c75a6f4
mcarolmartins <carol-martins7@hotmail.com>
2021-03-18 21:49:29 -0300

    criação de banco de dados com mirageJS e integração

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Celular Galaxy S10+',
+          type: 'withdraw',
+          category: 'Compras',
+          amount: 2100,
+          createdAt: new Date('2021-03-06 09:30:00'),
+        },
+        {
+          id: 2,
+          title: 'Desenvolvimento de sistema para Android',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 5300,
+          createdAt: new Date('2021-03-10 19:00:00'),
+        },
+        {
+          id: 3,
+          title: 'Cartão de crédito',
+          type: 'withdraw',
+          category: 'Pagamentos',
+          amount: 400,
+          createdAt: new Date('2021-03-12 14:30:00'),
+        },
+        {
+          id: 4,
+          title: 'Troca de HD, fonte e memória RAM',
+          type: 'deposit',
+          category: 'Recebimentos',
+          amount: 150,
+          createdAt: new Date('2021-03-12 22:50:00'),
+        },
+      ],
+    })
+  },
   routes() {
     this.namespace = 'api';
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(),
-        }
-      ]
-    }
-  )}
-})
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data);
+    })
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
